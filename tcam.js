f2e07dc45d2433cca62e33f4808d86dfe13c6329
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-02-01 00:18:13 +0100

    Update tcam.js

--- tcam.js
+++ tcam.js
@@ -1,4 +1,16 @@
-function getTipClicked() {
+class getTipClicked {
+    constructor(el) {
+        this.el = el;
+    }
+    handleEvent(event) {
+        switch(event.target) {
+            case '0': 
+
+        }
+    }
+}
+
+/*function getTipClicked() {
     let tip = document.getElementsByClassName('ci-tip-grid')[0].querySelectorAll('button');
     tip.forEach(function(check) {
         check.addEventListener('click', function(event) {
@@ -6,7 +18,7 @@ function getTipClicked() {
            //console.log(y);
         });
     });
-}
+}*/
 
 function getFromCalc() {
     let bill = document.getElementsByClassName('ci-bill')[0].querySelector('input').value;
@@ -15,4 +27,4 @@ function getFromCalc() {
 }
 
 
-// Turn clicked event into object by Class and use inheriting in the constructor to get something from method
+// Turn clicked event into object by Class and use inheriting in the constructor to get something from method or simply get a return from function calling eventhandler merging into call 
