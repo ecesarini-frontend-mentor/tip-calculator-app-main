968f20dd8374dc1bcb38ab391da010afcad39be8
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-02-02 00:29:48 +0100

    Update tcam.js

--- tcam.js
+++ tcam.js
@@ -1,30 +1,51 @@
-class getTipClicked {
+class TipClicked {
     constructor(el) {
         this.el = el;
     }
     handleEvent(event) {
-        switch(event.target) {
-            case '0': 
-
+        var ret;
+        var y = Array.from(this.el).indexOf(event.target);
+        switch (y) {
+            case '0':
+                ret = .05;
+                break;
+            case '1':
+                ret = .1;
+                break;
+            case '2':
+                ret = .15;
+                break;  
+            case '3':
+                ret = .25;
+                break;  
+            case '4':
+                ret = .5;
+                break;  
+            case '5':
+                ret = .8;
+                break;          
         }
-    }
+    } 
 }
 
-/*function getTipClicked() {
+function getTipClicked() {
     let tip = document.getElementsByClassName('ci-tip-grid')[0].querySelectorAll('button');
-    tip.forEach(function(check) {
-        check.addEventListener('click', function(event) {
-           var y = Array.from(tip).indexOf(event.target);
+    let tipClicked = new TipClicked(tip);
+    console.log(tipClicked);
+    /*tip.forEach(function(check) {
+        check.addEventListener('click', tipClicked.checkClicked);
+           //var y = Array.from(tip).indexOf(event.target);
            //console.log(y);
-        });
-    });
-}*/
+        //);
+    //console.log(y);
+    });*/
+}
 
 function getFromCalc() {
     let bill = document.getElementsByClassName('ci-bill')[0].querySelector('input').value;
     getTipClicked();
-    console.log(y);
+    //console.log(y);
 }
-
+getFromCalc();
 
 // Turn clicked event into object by Class and use inheriting in the constructor to get something from method or simply get a return from function calling eventhandler merging into call 
