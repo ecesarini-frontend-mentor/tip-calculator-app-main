4db36cc8c4cedf7d1b7d40e1eae570ca7b9a8328
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-02-13 00:04:41 +0100

    commit

--- tcam.js
+++ tcam.js
@@ -1,68 +1,72 @@
-class GetValue {
-    constructor(bill, tip, tipPerc) {
-        this.bill = bill;
+class GetTipValue {
+    constructor(tip, c1, c2) {
         this.tip = tip;
-        this.tipPerc = tipPerc;
+        this.c1 = c1;
+        this.c2 = c2;
     }
     handleEvent(event) {
-        //console.log(event.target);
         let tipClkIndex = Array.from(this.tip).indexOf(event.target);
-        //console.log(tipClkIndex);
         if(tipClkIndex < 5) { 
-            //document.getElementsByClassName('ci-tg-span')[0].style.backgroundColor = '';
-            //document.getElementsByClassName('ci-tg-span')[0].innerText;            
+            let lc = event.target.parentNode.lastElementChild;
             switch(event.type) {
                 case 'click':
-                    let perc = this.tipPerc[tipClkIndex];
-                    console.log(this.tipPerc[tipClkIndex]);
+                    //let perc = this.tipPerc[tipClkIndex];
+                    lc.style.backgroundColor = '';
+                    break;
                 }
         } else if(tipClkIndex === 5) {
-            console.log(event.target.value);
             let etvNotEmpty = () => { if(event.target.value != '') return true; }
             switch(event.type) {
-                //case 'click':
-                    //if(etvNotEmpty() || !isNaN(event.target.value)) event.target.value = 'Custom';
-                    //if(event.target.value == 'Custom') event.target.value = '';
-                    //break;
-                case 'focus':
-                    event.target.value = '';
+                case 'click':
+                    event.target.style.borderColor = this.c1;
+                    if(etvNotEmpty() || !isNaN(event.target.value)) event.target.value = '';
+                    break;
+                case 'input':
+                    event.target.style.borderColor = this.c1;
+                    if(isNaN(event.target.value)) event.target.value = event.target.value.slice(0,-1);
+                    if(event.target.value < 0) event.target.value = 0;
+                    else if (event.target.value > 100) event.target.value = 100;
                     break;
                 case 'blur':
                     if(etvNotEmpty()) { 
                         event.target.value = event.target.value + '%';
-                        event.target.style.color = 'hsl(183, 100%, 15%)';
-                    } 
-                    break;
-                case 'input':
-                    //event.target.style.textAlign = 'center';
-                    //event.target.value = '';
-                    if(isNaN(event.target.value)) event.target.value = event.target.value.slice(0,-1);
-                    if(event.target.value < 0) event.target.value = 0;
-                    else if (event.target.value > 100) event.target.value = 100;
-                    //if(etvNotEmpty()) event.target.value += event.target.value;
-                    //if(etvNotEmpty()) event.target.parentElement.style.backgroundColor = 'hsl(172, 67%, 45%)';
+                        event.target.style.color = this.c2;
+                        event.target.style.backgroundColor = this.c1;
+                    } else { 
+                        event.target.value = 'Custom'; 
+                        event.target.backgroundColor = '';
+                    }
                     break;
             }
         }
     }
 }
-class DisplayResult {
-    constructor() {}
 
-
+function getTipClicked(tip, c1) {
+    tip.forEach((item) => {
+        console.log(item.style);//.style.backgroundColor);
+        //return item.style.backgroundColor;
+        //if(item.style.backgroundColor == c1) return console.log(item);
+    });
+    //if(this.tip.style.backgroundColor == this.c1) {
+        //return this.tip.value;
+    //}
 }
+
 function tcamValue() {
     let bill = document.getElementsByClassName("ci-bill-input")[0].value;
     let tip = document.querySelectorAll(".ci-tg-perc");
-    //console.log(tip);
+    let c1 = 'hsl(172, 67%, 45%)';
+    let c2 = 'hsl(183, 100%, 15%)';
     let tipPerc = [0.05, 0.1, 0.15, 0.25, 0.5];
 
-    let getValue = new GetValue(bill, tip, tipPerc);
+    let getTipValue = new GetTipValue(tip, c1, c2);
     tip.forEach((item) => {
-        item.addEventListener('click', getValue);
-        item.addEventListener('input', getValue);
-        item.addEventListener('blur', getValue);
+        item.addEventListener('click', getTipValue);
+        item.addEventListener('input', getTipValue);
+        item.addEventListener('blur', getTipValue);
     }) 
+    console.log(getTipClicked(tip, c1));
     // TODO: fetch data inside DOM input: 
 }
 tcamValue();
