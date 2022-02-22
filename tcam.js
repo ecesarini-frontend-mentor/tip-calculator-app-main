d7f5d263aa5dd035ba9e1aa200ee853f20487c7f
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-02-22 01:25:19 +0100

    Update tcam.js

--- tcam.js
+++ tcam.js
@@ -3,16 +3,12 @@ class ManageTips {
         this.tip = tip;
         this.c1 = c1;
         this.c2 = c2;
-        //this.fo = fo;
     }
     handleEvent(event) {
-        //console.log(event.target.className.split(" "));
-        //console.log(typeof(event.target.className));
         let etvNotEmpty = () => { 
             if(event.target.value != '') return true; 
             else return false;
         }
-        //console.log(event.target, event.currentTarget);
         if(event.target.type === 'button') { 
             event.target.parentNode.lastElementChild.style = '';
             event.target.parentNode.lastElementChild.value = 'Custom';
@@ -44,22 +40,37 @@ class ManageTips {
     }
 }
 
-function calcAmounts(e) {
-    var focused;
-    let evSpl = e.target.className.split(" ");
-    /*let etvFilled = () => { 
-        if(event.target.value != '' && isNaN(event.target.value.slice(0,-1))) return true; 
-        else return false;
-    }*/
-    //if(evSpl.includes("ci-bill-input") || evSpl.includes("ci-people-input") || evSpl.includes("ci-"))
-    //if(!evSpl.includes("ci-tg-input-custom")) 
-    focused = document.activeElement.value;
-    if(e.type === 'focus' && e.target.type === 'button') focused = document.activeElement.value.slice(0,-1)*0.01;
-    //else focused = document.activeElement.value;
+class Calc {
+    constructor(bill, tip, people) {
+        this.bill = bill;
+        this.tip = tip;
+        this.people = people;
+    }
 
-    //else if(evSpl.includes("ci-tg-input"))
-    //    focused = document.activeElement.value;
-    console.log(focused);
+    handleEvent(event) {
+        let cArr = Array(3);
+        //if(event.type === 'input') console.log(event.target.className, event.target.value, event.target.value);
+        if(event.type === 'input') {
+            let className = event.target.className;
+            let checkClassName = (name) => { 
+                //console.log(className);
+                if(className.indexOf(name) !== -1) return true; 
+                else return false;
+            }
+            switch(className) {
+                case checkClassName('ci-bill-input'):
+                    cArr[0] = event.target.value;
+                    break;
+                case checkClassName('ci-tg-input-custom'):
+                    cArr[1] = event.target.value;
+                    break;
+                case checkClassName('ci-tg-people'):
+                    cArr[2] = event.target.value;
+                    break;
+            }
+            console.log(cArr);
+        }
+    }   
 }
 
 function tcamValue() {
@@ -68,24 +79,23 @@ function tcamValue() {
     let people = document.getElementsByClassName("ci-people-amount")[0];
     let c1 = 'hsl(172, 67%, 45%)';
     let c2 = 'hsl(183, 100%, 15%)';
-    var selected;
 
-    //console.log(tip);
     let manageTips = new ManageTips(tip, c1, c2);
     tip.forEach((item) => {
-        console.log("manageTips:\n" + "\nthis: " + this+ "item: " + item);  // TODO: test _closure_ learning :|
-        //item.addEventListener('click', manageTips);
-        //tip.addEventListener('focus', clearTipBg);
+        //console.log("manageTips:\n" + "\nthis: " + this + "item: " + item);  // TODO: test _closure_ learning :|
         item.addEventListener('click', manageTips);
         item.addEventListener('focus', manageTips);
-        item.addEventListener('focus', calcAmounts);
+        //item.addEventListener('focus', calcAmounts);
         item.addEventListener('input', manageTips);
         item.addEventListener('blur', manageTips);
     });
 
-    bill.addEventListener('input', calcAmounts);
+    let calc = new Calc(bill, tip, people);
+    [bill, people].forEach((item) => item.addEventListener('input', calc));
+
+    /*bill.addEventListener('input', calcAmounts);
     document.getElementsByClassName("ci-tip-grid")[0].addEventListener('input', calcAmounts);
     //document.getElementsByClassName("ci-tip-grid")[0].addEventListener('focus', calcAmounts);
-    people.addEventListener('input', calcAmounts);
+    people.addEventListener('input', calcAmounts);*/
 }
 tcamValue();
