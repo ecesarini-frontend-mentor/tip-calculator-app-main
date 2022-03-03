4ac64f6511ee5f176ac57b45b768c04619270284
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-03-04 00:01:58 +0100

    using localStorage['param'} to handle event data

--- tcam.js
+++ tcam.js
@@ -1,111 +1,90 @@
-class ManageTips {
-    constructor(tip, c1, c2) {
+class ManageCalc {
+    constructor(bill, tip, people, c1, c2, billClassName, tipClassName, tipCustomClassName, peopleClassName) {
+        this.bill = bill;
         this.tip = tip;
+        this.people = people;
         this.c1 = c1;
         this.c2 = c2;
+        this.billClassName = billClassName;
+        this.tipClassName = tipClassName;
+        this.tipCustomClassName = tipCustomClassName;
+        this.peopleClassName = peopleClassName;
     }
+    
     handleEvent(event) {
-        let etvNotEmpty = () => { 
-            if(event.target.value != '') return true; 
+        //let selectedClassName = event.target.className.split(" ");
+        let checkSelectedClassName = (cn) => { 
+            if(event.target.className.split(" ").indexOf(cn) != -1) return true;
             else return false;
+        };
+
+        if(checkSelectedClassName(this.billClassName)) {
+            if(event.type === 'input') localStorage['bill'] = event.target.value;
         }
-        if(event.target.type === 'button') { 
-            event.target.parentNode.lastElementChild.style = '';
-            event.target.parentNode.lastElementChild.value = 'Custom';
-        }
-        else if(event.target.type === 'text') {
-            switch(event.type) {
-                case 'focus':
-                    event.target.style.borderColor = this.c1;
-                    if(etvNotEmpty() || !isNaN(event.target.value)) event.target.value = '';
+        else if(checkSelectedClassName(this.tipClassName)) {
+            let etvTipNotEmpty = () => { 
+                if(event.target.value != '') return true; 
+                else return false;
+            }            
+            switch(event.type) { // TODO: check the event call
+                case 'button':
+                    event.target.parentNode.lastElementChild.style = '';
+                    event.target.parentNode.lastElementChild.value = 'Custom';
+                    //if(event.target.type === 'click' || event.type === 'blur') event.target.style.backgroundColor = this.c1;
+                    //if(event.target.type === 'click') 
+                    event.target.style.backgroundColor = this.c1; // TODO: check here, update handler because not-custom field of tips doesn't update style
+                    localStorage['tip'] = event.target.value.slice(0,-1);
                     break;
-                case 'input':
-                    event.target.style.borderColor = this.c1;
-                    if(isNaN(event.target.value)) event.target.value = event.target.value.slice(0,-1);
-                    if(event.target.value < 0) event.target.value = 0;
-                    else if(event.target.value > 100) event.target.value = 100;
-                    break;
-                case 'blur':
-                    if(etvNotEmpty()) { 
-                        event.target.value = event.target.value + '%';
-                        event.target.style.color = this.c2;
-                        event.target.style.backgroundColor = this.c1;
-                    } else { 
-                        event.target.value = 'Custom'; 
-                        event.target.backgroundColor = '';
+                case 'text':
+                    switch(event.target.type) { //TODO: 
+                        case 'focus':
+                            event.target.style.borderColor = this.c1;
+                            if(etvTipNotEmpty() || !isNaN(event.target.value)) event.target.value = '';
+                            break;
+                        case 'input':
+                            event.target.style.borderColor = this.c1;
+                            if(isNaN(event.target.value)) event.target.value = event.target.value.slice(0,-1);
+                            if(event.target.value < 0) event.target.value = 0;
+                            else if(event.target.value > 100) event.target.value = 100;
+                            localStorage['tip'] = event.target.value;
+                            break;
+                        case 'blur':
+                            if(etvTipNotEmpty()) { 
+                                event.target.value = event.target.value + '%';
+                                event.target.style.color = this.c2;
+                                event.target.style.backgroundColor = this.c1;
+                            } else { 
+                                event.target.value = 'Custom'; 
+                                event.target.backgroundColor = '';
+                            }
+                            break;
                     }
-                    break;
-            }
-        }   
-    }
-}
-
-// ref https://stackoverflow.com/questions/16053866/javascript-pass-multiple-arguments-to-eventlistener-and-the-event
-var calcAmount = function(b, st, p) {  // calcAmount is a function that accepts 4 arguments and return the listener
-    return function curriedFunction(e) {  // 'addEventListener' 2nd argument 'callback listener' accepts one argument (event)
-        //calcAmount();  <--- perche' e' stata chiamata questa funzione nella ref?
-        //let et = e.type;
-        let billVal = b.value;
-        let tipVal;
-        let peopleVal = p.value;
-        let etcn = e.target.className.split(" "); // is "let" right?
-        //console.log(etcn);
-        var etcnCheck = (etcn, name) => {
-            let chk; 
-            if(etcn.indexOf(name) != -1) chk = true;
-            else chk = false;
-            return chk;
-        }; 
-        console.log(etcn, etcnCheck(etcn,"ci-tg-input"), etcnCheck(etcn, "ci-tg-input-custom"));
-        if(etcnCheck(etcn, "ci-tg-input") && !etcnCheck(etcn, "ci-tg-input-custom")) {
-            if(e.type == 'input') {
-                //console.log("input");
-                if(st) {st = null;}
-                st = e.target.value;
+                    break;    
             }
         }
-        else if(etcnCheck(etcn, "ci-tg-input")) {
-            if(e.type == 'click') {
-                //co
-                if(st) {st = null;}
-                st = e.target.value.slice(0,-1);
-            }
+        else if(checkSelectedClassName(this.peopleClassName)) {
+            if(event.type === 'input') localStorage['people'] = event.target.value;
         }
-        //console.log(billVal,peopleVal,st);
-        /*if(etcn.indexOf("ci-tg-input-custom")  != -1) {
-            if(e.target.className.split(" ").length > 1) {
-                tipVal = e.target.value 
-            }
-        }*/
-        /*let etcn = e.target.className.split(" ").indexOf("ci-tg-input"); // TODO: try here, to understand the catch about listener
-        if(etcn != -1) {
-            if(etcn.indexOf("ci-tg-input-custom") != -1 && et == "input") tipVal = e.target.value;
-            else tipVal = e.target.value.slice(0,-1);
-        }*/
     }
 }
-
 
 function tcamValue() {
     let bill = document.getElementsByClassName("ci-bill-input")[0];
     let tip = document.querySelectorAll(".ci-tg-input");
-    let selectTip;
     let people = document.getElementsByClassName("ci-people-input")[0];
     let c1 = 'hsl(172, 67%, 45%)';
     let c2 = 'hsl(183, 100%, 15%)';
-    let manageTips = new ManageTips(tip, c1, c2);
-    let evtArray = ['input', 'click', 'blur'];
-    let cA = calcAmount(bill, selectTip, people);
+    let manageCalc = new ManageCalc(bill, tip, people, c1, c2, 'ci-bill-input', 'ci-tg-input', 'ci-people-input');
 
+    bill.addEventListener('input', manageCalc);
     tip.forEach((item) => {
-        item.addEventListener('click', manageTips);
-        item.removeEventListener('click', manageTips);
-        item.addEventListener('focus', manageTips);
-        //item.removeEventListener('focus', manageTips);
-        item.addEventListener('input', manageTips);
-        item.addEventListener('blur', manageTips);
+        item.addEventListener('click', manageCalc);
+        item.addEventListener('click', manageCalc);
+        item.addEventListener('focus', manageCalc);
+        item.addEventListener('input', manageCalc);
+        item.addEventListener('blur', manageCalc);
     });
-    ['input', 'click', ].forEach((ev) =>  {document.addEventListener(ev, cA) });
+    people.addEventListener('input', manageCalc);
 }
 
 tcamValue();
